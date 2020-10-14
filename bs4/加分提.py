db4338a0522f5948995415d40a7b4d920669ee05
TBJ123 <72490396+TBJ123@users.noreply.github.com>
2020-10-14 16:22:10 +0800

    Add files via upload

---
+++ bs4/加分提.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from bs4 import BeautifulSoup
+import requests
+url = 'https://www.chinatimes.com/?chdtv'
+html = requests.get(url)
+soup = BeautifulSoup(html.text,'lxml')
+titles = soup.find_all('section','focus-news')
+
+for title in titles:
+    a_tag = title.find_all('a')
+    for tag in a_tag:
+        a_href = tag.get('href')
+        print(tag.text+'\n'+"網址: "+a_href)
+        print('\n')
+        
+    
+    
+    
+
+
+# In[ ]:
+
+
+
+
